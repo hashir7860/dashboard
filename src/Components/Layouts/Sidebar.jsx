cc7e92d8fd89348c98da4a1d5c02fd3a02989b7b
hashir-it <m.hashir.it@gmail.com>
2023-12-27 17:03:23 +0500

    use used state

--- src/Components/Layouts/Sidebar.jsx
+++ src/Components/Layouts/Sidebar.jsx
@@ -1,53 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiBars3, RiApps2Line } from "react-icons/hi2";
 
 const Sidebar = () => {
+    const[isOpen ,setIsOpen]=useState(true);
+    const toogle = () => setIsOpen (!isOpen);
+
     return (
         <div className='left-sidebar'>
-            <div className="p-3 sidebar" id="mySidebar">
+            <div style={{width: isOpen ? "200px" : "50px"}} className="p-3 sidebar" id="mySidebar">
                 <div className="sidebar-toogle-container">
                     <span id="sidebar-toogle" role="button">
-                        ☰
+                    <HiBars3    onClick={toogle}/>
                     </span>
                 </div>
-                <div className="logo border-bottom text-center pt-3 pb-4">Logo</div>
-                <ul className="list-unstyled p-3">
-                    <li className="p-2 mt-4 active">
-                        <a href="#" className="text-decoration-none d-flex">
+                <div className="logo border-bottom text-center pt-3 pb-4" style={{display: isOpen ? "block" : "none"}}>Logo</div>
+                <ul className="list-unstyled p-3 ps-0">
+                    <li className="p-2 ps-0 mt-4 text">
+                        <a href="#" className="text-decoration-none d-flex p-2 ps-0 active">
                             <img src="Assets/Dashboard.svg" alt="" className="pe-3" />
-                            <span>Dashboard</span>
+                            Dashboard
                         </a>
                     </li>
-                    <li className="p-2 mt-4">
+                    <li className="p-2 ps-0 mt-4">
                         <a href="inventory.html" className="text-decoration-none d-flex">
                             <img src="Assets/Inventory.svg" className="pe-3" alt="" />
                             <span>Inventory</span>
                         </a>
                     </li>
-                    <li className="p-2 mt-4">
+                    <li className="p-2 ps-0 mt-4">
                         <a href="#" className="text-decoration-none d-flex">
                             <img src="Assets/Analytics.svg" className="pe-3" alt="" />
                             <span>Analytics</span>
                         </a>
                     </li>
-                    <li className="p-2 mt-4">
+                    <li className="p-2 ps-0 mt-4">
                         <a href="alerts.html" className="text-decoration-none  d-flex">
                             <img src="Assets/Allerts.svg" className="pe-3" alt="" />
                             <span>Alerts</span>
                         </a>
                     </li>
-                    <li className="p-2 mt-4">
+                    <li className="p-2 ps-0 mt-4">
                         <a href="recomendations.html" className="text-decoration-none d-flex">
                             <img src="Assets/Recommendations.svg" className="pe-3" alt="" />
                             <span>Recommendation</span>
                         </a>
                     </li>
-                    <li className="p-2 mt-4 ">
+                    <li className="p-2 ps-0 mt-4 ">
                         <a href="feedback.html" className="text-decoration-none d-flex">
                             <img src="Assets/Feedback.svg" className="pe-3" alt="" />
                             <span>Feedback</span>
                         </a>
                     </li>
-                    <li className="p-2 logout">
+                    <li className="p-2 ps-0 logout">
                         <a href="#" className="text-decoration-none d-flex">
                             <img src="Assets/Logout.svg" className="pe-3" alt="" />
                             <span>Logout</span>
@@ -60,31 +64,4 @@ const Sidebar = () => {
     )
 }
 
-
-const sidebar = document.querySelector('.sidebar');
-const sidebarButtonToogle = document.querySelector('#sidebar-toogle');
-const sidebarItemsText = document.querySelectorAll('.sidebar-item-text');
-
-sidebarButtonToogle.addEventListener('click', (event) => {
-    // Add-Remove Expand Class - Width Handler
-    sidebar.classList.toggle('expand-sidebar');
-
-    if (sidebar.classList.contains('expand-sidebar')) {
-        toogleSidebarItemtext();
-    } else {
-        // Wait until Transition-Animation finish
-        setTimeout(() => {
-            toogleSidebarItemtext();
-        }, 100);
-    }
-});
-
-/**
- * Show-Hide Items Text
- */
-function toogleSidebarItemtext() {
-    sidebarItemsText.forEach((itemText) => {
-        itemText.classList.toggle('hidden');
-    });
-}
 export default Sidebar
